d7f2df27d6d850c2008b549fd432aace548a7213
nchua3012 <n.chua3012@googlemail.com>
2025-02-24 15:40:58 +0000

    Changes to form / Data to form submitted to local storage / new HTML pages set up for living room page

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
 // Define a route for the home page
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/Front-end/public/index.html');
+});
+
+// Serve static files from the "public" directory
+app.use(express.static('Front-end/public'));
+
+// Route for create.html
+app.get('/create.html', (req, res) => {
+	res.sendFile(__dirname + 'Front-end/public/create.html');
 });
 
 // Start the server
