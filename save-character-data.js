773d4a44987bf6ad8e5f7e983277faca8800d96b
DNMathEye <101174369+DNMathEye@users.noreply.github.com>
2025-03-04 09:32:43 +0000

    json updates

--- save-character-data.js
+++ save-character-data.js
@@ -15,7 +15,7 @@ const client = new OpenAI({
 //JM defining a path for get request
 router.get("/characters", (req, res) => {
   //JM saving route to character folder in dataFolder variable
-  const dataFolder = path.join(__dirname, "back-end", "characterData")
+  const dataFolder = path.join(__dirname, "back-end", "character_data")
 
   //JM checking for files in the folder --> parameters are options
   fs.readdir(dataFolder, (err, files) => {
@@ -66,12 +66,16 @@ router.post("/characters", async (req, res) => {
     const fileName = `${characterData.name.replace(/\s+/g, "_")}_character.json`
 
     //NC - path to save JSON files in a folder
-    const dataFolder = path.join(__dirname, "back-end", "characterData")
+    //DM - edited to add archive folder
+    const archiveFolder = path.join(__dirname, "back-end", "character_archive")
+    const dataFolder = path.join(__dirname, "back-end", "character_data")
 
     //NC - Define the full path to the file - required as part using the file save
-    const filePath = path.join(dataFolder, fileName)
-
+    const filePath = path.join(archiveFolder, fileName)
+    const filePath2 = path.join(dataFolder, "character_list.json")
+    
     //NC - Write the character data extracted from the form to a file in JSON format
+    //DM changed filePath to save to character archive, which will be ignored by git, to store generated characters
     fs.writeFile(
       filePath,
       JSON.stringify(fullCharacterData, null, 2),
@@ -90,6 +94,18 @@ router.post("/characters", async (req, res) => {
         })
       }
     )
+
+    //DM - implementing adding new chars to the character list JSON
+    //DM - note, using Sync version - will investigate difference without (error catching I think)
+    let charlistjson = fs.readFileSync(filePath2)
+    let charlist = JSON.parse(charlistjson)
+    const arrcharlist = Array.from(charlist)
+    arrcharlist.push(fullCharacterData)
+    charlist = { ...arrcharlist}
+    charlistjson = JSON.stringify(charlist)
+    fs.writeFileSync(filePath2, charlistjson)
+
+    // fs.readFileSync()
   } catch (error) {
     console.error("error saving character data:", error)
   }
