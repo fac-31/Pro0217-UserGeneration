b91d586a70418c5de62fef96b4444f321e915865
nchua3012 <n.chua3012@googlemail.com>
2025-03-05 16:39:26 +0000

    updates to intruder/battle

--- back-end/battle-type1.js
+++ back-end/battle-type1.js
@@ -1,7 +1,7 @@
 //NC - hobby horse race function
 //NC - if the kitty litter launcher is detected a comparison between charisma is triggered
 
-const intruder = require("./selecting-intruder");
+const intruder = require("./selecting-intruder.js");
 const currentUser = require("./retrieve-user-saved-JSON.js");
 
 async function hobbyHorseRace() {
@@ -10,32 +10,44 @@ async function hobbyHorseRace() {
         const character2 = await intruder(); 
 
         let hobbyHorseWinner;
+        let hobbyHorseLoser;
 
 //Compare Charisma to determine the winner
         if (character1.Charisma > character2.Charisma) {
             hobbyHorseWinner = character1;
+            hobbyHorseLoser = character2;
+
         } else if (character2.Charisma > character1.Charisma) {
             hobbyHorseWinner = character2;
+            hobbyHorseLoser = character1;
+
         } else {
             hobbyHorseWinner = null;
+            hobbyHorseLoser = null;
         }
 
         if (hobbyHorseWinner){
             return {
                 message:"Winner!",
-                winner: hobbyHorseWinner
+                winner: {
+                    name: hobbyHorseWinner.name,
+                    charisma: hobbyHorseWinner.Charisma
+                },
+                loser: {
+                    name: hobbyHorseLoser.name,
+                    charisma: hobbyHorseLoser.Charisma
+                }
             }
             } else {
                 return {
                     message: "draw!",
                     winner: null,
-                }
+                    loser: null
+                };
             }
-        }
-
-        catch (error) {
+        } catch (error) {
         console.error("Error getting a winner:", error);
         return null; 
     }
-}
-module.exports = hobbyHorseRace;
+}   
+module.exports = hobbyHorseRace
---
+++ back-end/generate-battle-tale.js
@@ -0,0 +1,37 @@
+//NC - openai call that creates a story of the battle
+
+async function generateBattleTale(client, winner, loser) {
+    console.log("Writing the epic tale...");
+
+    try {
+        let prompt;
+
+        // If there's a winner, create the prompt for the battle story
+        if (winner && loser) {
+            prompt = `Imagine yourself as J.R.R. Tolkien or G.R.R. Martin. Create an epic hobby horse racing tale between ${winner.name} and ${loser.name}. This battle was based on their charisma. The story should be at least 100 words long.`;
+        } else {
+            // If it's a draw, create a prompt for the draw scenario
+            prompt = `Imagine yourself as C.S. Lewis. Write an epic hobby horse racing tale that results in a draw between the two racers. The story should be at least 100 words long.`;
+        }
+
+        // Send the prompt to OpenAI API for completion
+        const completion = await client.chat.completions.create({
+            model: "gpt-4o-2024-08-06", 
+            messages: [
+                {
+                    role: "user",
+                    content: prompt
+                },
+            ],
+        });
+
+        // Return the story from the response
+        return completion.choices[0].message.content;
+
+    } catch (error) {
+        console.error("Error creating the battle story:", error);
+        return null;
+    }
+}
+
+module.exports = generateBattleTale
