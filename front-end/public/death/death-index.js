eef62977f685a769526cdf421f577134fdf49a32
nchua3012 <n.chua3012@googlemail.com>
2025-03-20 16:59:20 +0000

    tweaks to file paths

--- front-end/public/death/death-index.js
+++ front-end/public/death/death-index.js
@@ -2,7 +2,7 @@ import {deathcontent} from "./death-content.js";
 import formatEvents from "../../index/format-events.js";
 
 const mainContent = document.getElementById("mainContent");
-mainContent.style.backgroundImage = "url('./death/ghostTortoise.png')";
+mainContent.style.backgroundImage = "url('/death/ghostTortoise.png')";
 
 const contents = [deathcontent.youDead];
 
